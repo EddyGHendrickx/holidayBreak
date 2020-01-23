cc139f24bd87b5a4203c5e98e9d170299a833011
Matthijs Hendrickx <matthijs.hendrickx@gmail.com>
2020-01-23 11:02:59 +0100

    kinda works, but not really satisfied

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 armSeconds = document.getElementById("armSec");
-let i = 0;
-setInterval(function () {
+armMinutes = document.getElementById("armMin");
+armHours = document.getElementById("armHour");
 
-    armSeconds.setAttribute("style", `transform: rotate(${i*6}deg)`);
-    if(i === 61){
-        i = 0;
-        i++;
+
+let s = 50;
+let m = 59;
+let h = 0;
+//Seconds Timer
+setInterval(function () {
+    armSeconds.setAttribute("style", `transform: rotate(${s * 6}deg)`);
+    if (s === 60) {
+        minutePasses();
+        s = 0;
+        s++;
     } else {
-        i++;
+        s++;
     }
 }, 1000);
+
+//Minutes Timer
+function minutePasses() {
+    m++;
+    armMinutes.setAttribute("style", `transform: rotate(${m * 6}deg)`);
+    if (m === 60) {
+        hourPasses();
+        m = 0;
+    }
+}
+
+function hourPasses() {
+    h++;
+    armHours.setAttribute("style", `transform: rotate(${h * 30}deg)`);
+    if (h === 60){
+        h = 0;
+    }
+}
